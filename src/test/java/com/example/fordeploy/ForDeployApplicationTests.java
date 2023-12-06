9226733e584bbed82a095ee8a787c203a4e15ee9
Jamshidbek Karimov <jamshidbek_karimov1@epam.com>
2023-12-06 18:15:52 +0500

    Initial commit

---
+++ src/test/java/com/example/fordeploy/ForDeployApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.fordeploy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ForDeployApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
